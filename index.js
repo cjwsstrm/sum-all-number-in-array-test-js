f430706fe35cdab653ce82e1ff565c44b4fe4071
cjwsstrm <cjwesstrom@gmail.com>
2018-01-23 13:47:56 -0800

    function now sums numbers from nested arrays correctly

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
 }
+
+const array = [1, [[2, [3]], [[[4]], 5]]];
+sumItems(array);
+
 
 module.exports = sumItems;
